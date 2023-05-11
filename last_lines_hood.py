5e342a15e52c3d6c130dd879453f612568a32ff5
abigailjmiller <118693385+abigailjmiller@users.noreply.github.com>
2023-05-11 15:49:46 -0700

    python script using pandas to print last lines of my button files

---
+++ last_lines_hood.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+import re
+import sys
+import os
+import pandas as pd
+
+
+last_lines = open("last_lines_hood.csv", "a")
+directory = 'New_hood' #define the directory
+for filename in os.listdir(directory): #for each file in directory, do this
+	if filename.endswith('.csv'): #if the file ends in csv open it
+		with open(os.path.join(directory, filename)) as f: 
+			df = pd.read_csv(f)
+			bottom = df.tail(1)
+			print(bottom, file=last_lines)
+			
